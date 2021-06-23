840573826f7e5251b65e08189352c00f59839c45
Marisa Ramon <mer0522@gmail.com>
2021-06-23 14:05:40 -0500

    revert to ruby 2.4 to make tests pass

--- app/api.rb
+++ app/api.rb
@@ -4,12 +4,28 @@ require 'json'
 module ExpenseTracker
   class API < Sinatra::Base
 
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
+    end
+
+    post '/expenses' do
+      # status 404
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
+    end
+
+
     get '/expenses/:date' do 
       JSON.generate([])
     end
-    
-    post '/expenses' do
-      JSON.generate('expense_id' => 42)
-    end
+
+
   end
 end
